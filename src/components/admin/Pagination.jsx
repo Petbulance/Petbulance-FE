cd29a035e20c503b0fdb4866ebbd232d9c7206cd
mg <bidal.mingyu@gmail.com>
2025-12-27 21:22:30 +0900

    style : 페이징 디자인 수정

--- src/components/admin/Pagination.jsx
+++ src/components/admin/Pagination.jsx
@@ -1,13 +1,17 @@
 import React, { useMemo } from 'react';
 
 export default function Pagination({
-  page,
-  totalPages,
-  onChange,
-  groupSize = 10,
-}) {
-  const pageGroupStart = Math.floor((page - 1) / groupSize) * groupSize + 1;
-  const pageGroupEnd = Math.min(pageGroupStart + groupSize - 1, totalPages);
+                                     page,
+                                     totalPages,
+                                     onChange,
+                                     groupSize = 10,
+                                   }) {
+  const pageGroupStart =
+    Math.floor((page - 1) / groupSize) * groupSize + 1;
+  const pageGroupEnd = Math.min(
+    pageGroupStart + groupSize - 1,
+    totalPages
+  );
 
   const pageNumbers = useMemo(
     () =>
@@ -19,12 +23,18 @@ export default function Pagination({
   );
 
   return (
-    <div className="flex items-center gap-1 text-xs">
-      {/* 이전 그룹 */}
+    <div className="flex items-center gap-1 text-sm">
+      {/* 이전 페이지 */}
       <button
-        className="rounded border px-3 py-1.5 disabled:opacity-40"
-        onClick={() => onChange(Math.max(1, pageGroupStart - 1))}
-        disabled={pageGroupStart === 1}
+        onClick={() => onChange(page - 1)}
+        disabled={page === 1}
+        className="
+          rounded-md px-2 py-1
+          text-black-500
+          hover:bg-blue-50
+          disabled:opacity-30
+          transition
+        "
       >
         {'<'}
       </button>
@@ -33,21 +43,30 @@ export default function Pagination({
         <button
           key={num}
           onClick={() => onChange(num)}
-          className={`rounded border px-3 py-1.5 transition ${
+          className={`
+            rounded-md px-3 py-1.5 transition
+            ${
             num === page
-              ? 'border-blue-600 bg-blue-600 text-white'
-              : 'hover:bg-white'
-          }`}
+              ? 'bg-blue-600 text-white'
+              : 'text-gray-600 hover:bg-gray-100'
+          }
+          `}
         >
           {num}
         </button>
       ))}
 
-      {/* 다음 그룹 */}
+      {/* 다음 페이지 */}
       <button
-        className="rounded border px-3 py-1.5 disabled:opacity-40"
-        onClick={() => onChange(Math.min(totalPages, pageGroupEnd + 1))}
-        disabled={pageGroupEnd === totalPages}
+        onClick={() => onChange(page + 1)}
+        disabled={page === totalPages}
+        className="
+          rounded-md px-2 py-1
+          text-black-500
+          hover:bg-blue-50
+          disabled:opacity-30
+          transition
+        "
       >
         {'>'}
       </button>
