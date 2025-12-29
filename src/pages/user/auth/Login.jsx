9fdf82cfe3c6ed9ce07cde15f0135681448afb69
mg <bidal.mingyu@gmail.com>
2025-12-29 19:49:48 +0900

    feat : apple -> naver 변경

--- src/pages/user/auth/Login.jsx
+++ src/pages/user/auth/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '@/assets/images/logo/pet_logo.svg';
 import kakaoLogo from '@/assets/images/logo/kakao-svgrepo-com.svg';
 import googleLogo from '@/assets/images/logo/google-color-svgrepo-com.svg';
-import appleLogo from '@/assets/images/logo/apple-173-svgrepo-com.svg';
+import naverLogo from '@/assets/images/logo/NAVER_LOGO.png';
 
 export default function Login() {
   return (<div className="flex min-h-screen flex-col items-center justify-center bg-white px-6">
@@ -16,17 +16,16 @@ export default function Login() {
     </div>
 
     {/* 문구 */}
-    <p className="mb-2 text-sm text-gray-500 text-center">
-      검증된 특수동물 병원 정보부터<br />
-      집사들의 실사용 후기 플랫폼까지,
+    <h2 className="mb-2 text-lg font-bold text-gray-950">
+      간편 로그인으로 더 다양한<br />
+      서비스를 이용해보세요!
+    </h2>
+    <p className="mb-5 text-sm text-tertiary text-center">
+      ⚡️ 3초 만에 빠른 회원가입
     </p>
 
-    <h2 className="mb-8 text-lg font-bold text-gray-900">
-      지금 <span className="text-emerald-600">펫블런스</span>에서 시작하세요!
-    </h2>
-
     {/* 버튼 영역 */}
-    <div className="w-full max-w-sm space-y-3">
+    <div className="w-full max-w-sm space-y-3 mt-10">
 
       {/* 카카오 */}
       <button
@@ -46,20 +45,23 @@ export default function Login() {
         구글로 시작하기
       </button>
 
-      {/* 애플 */}
+      {/* 네이버 */}
       <button
-        className="flex w-full items-center justify-start rounded-lg bg-[#1C1C1E] py-3 pl-30 text-sm font-semibold text-white">
-        <span className="mr-3 flex w-6 justify-center">
-          <img src={appleLogo} alt="apple" className="h-4 w-4 invert" />
-        </span>
-        애플로 시작하기
-      </button>
-      {/* 비회원 */}
-      <button
-        className="w-full rounded-lg border border-emerald-500 py-3 text-sm font-semibold text-emerald-600"
+        className="
+    flex w-full items-center justify-start
+    rounded-lg bg-[#03C75A]
+    py-3 pl-30
+    text-sm font-semibold text-white
+  "
       >
-        로그인 없이 둘러보기
+  <span className="mr-3 flex w-6 justify-center">
+    <img src={naverLogo} alt="naver" className="h-4 w-4" />
+  </span>
+        네이버로 시작하기
       </button>
+
+
+
     </div>
   </div>);
 }
