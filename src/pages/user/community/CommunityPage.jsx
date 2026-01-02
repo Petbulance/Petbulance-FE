af94810bab8f212a76935a7410a5583c75a14722
mg <bidal.mingyu@gmail.com>
2026-01-03 01:36:40 +0900

    style : 이미지 높이 설정

--- src/pages/user/community/CommunityPage.jsx
+++ src/pages/user/community/CommunityPage.jsx
@@ -2,8 +2,8 @@ import communityComingSoon from '@/assets/images/pageImages/community-coming-soo
 
 export default function CommunityPage() {
   return (
-    <div className="flex h-full flex-col px-6 text-center">
-      <div className="flex mt-50 flex-1 items-center justify-center">
+    <div className="flex mt-50 flex-col px-6 text-center">
+      <div className="flex items-center justify-center">
         <img
           src={communityComingSoon}
           alt="커뮤니티 준비중"
@@ -11,12 +11,12 @@ export default function CommunityPage() {
         />
       </div>
 
-      <div className="mt-10 pb-8">
-        <h2 className="mb-2 text-base font-semibold text-tertiary">
+      <div className="mt-5">
+        <h2 className="text-base font-semibold text-tertiary">
           커뮤니티 기능 준비중이에요!
         </h2>
 
-        <p className="text-sm text-tertiary">
+        <p className=" mt-3 text-sm text-tertiary">
           빠른 시일 내에 찾아뵐게요.
         </p>
       </div>
