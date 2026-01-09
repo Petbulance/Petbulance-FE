919a6c4599cad2c26e63a8f490f91f3fea755d43
yunseong0404 <333665@naver.com>
2026-01-09 12:17:53 +0900

    chore: 패키지 설치 및 수정

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
