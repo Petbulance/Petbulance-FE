16b4217c9aa555ff22b07acf226490b638eaa72c
yunseong0404 <333665@naver.com>
2026-01-30 18:45:24 +0900

    feat: data 추가

---
+++ src/data/animalSort.js
@@ -0,0 +1,53 @@
+export const ANIMAL_GROUPS = {
+  소형포유류: [
+    'HAMSTER',
+    'GUINEAPIG',
+    'CHINCHILLA',
+    'RABBIT',
+    'HEDGEHOG',
+    'FERRET',
+    'SUGAR_GLIDER',
+    'PRAIRIE_DOG',
+    'FLYING_SQUIRREL',
+    'OTHER_SMALL_MAMMALS',
+  ],
+  조류: ['PARROT', 'FINCH_TYPES', 'OTHER_BIRDS'],
+  파충류: ['GECKO', 'OTHER_LIZARDS', 'SNAKE', 'TURTLE', 'OTHER_REPTILES'],
+  양서류: ['FROG', 'AXOLOTL', 'SALAMANDER', 'OTHER_AMPHIBIANS'],
+  어류: ['ORNAMENTAL_FISH'],
+};
+
+export const ANIMAL_NAME_KO = {
+  // 소형포유류
+  HAMSTER: '햄스터',
+  GUINEAPIG: '기니피그',
+  CHINCHILLA: '친칠라',
+  RABBIT: '토끼',
+  HEDGEHOG: '고슴도치',
+  FERRET: '페럿',
+  SUGAR_GLIDER: '슈가글라이더',
+  PRAIRIE_DOG: '프레리독',
+  FLYING_SQUIRREL: '하늘다람쥐',
+  OTHER_SMALL_MAMMALS: '기타 소형포유류',
+
+  // 조류
+  PARROT: '앵무새',
+  FINCH_TYPES: '핀치류',
+  OTHER_BIRDS: '기타 조류',
+
+  // 파충류
+  GECKO: '게코',
+  OTHER_LIZARDS: '기타 도마뱀',
+  SNAKE: '뱀',
+  TURTLE: '거북이',
+  OTHER_REPTILES: '기타 파충류',
+
+  // 양서류
+  FROG: '개구리',
+  AXOLOTL: '우파루파',
+  SALAMANDER: '도롱뇽',
+  OTHER_AMPHIBIANS: '기타 양서류',
+
+  // 어류
+  ORNAMENTAL_FISH: '관상어',
+};
