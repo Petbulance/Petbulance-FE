3367d492290662dde0a13e71a7091da3a07f9e59
mg <bidal.mingyu@gmail.com>
2026-01-31 17:01:14 +0900

    refactor : 아이콘 변경

--- src/components/user/home/NearbyHospitalShortcut.jsx
+++ src/components/user/home/NearbyHospitalShortcut.jsx
@@ -1,9 +1,9 @@
-import allIcon from '@/assets/images/icons/icon-all.svg';
-import amphibianIcon from '@/assets/images/icons/icon-amphibian.svg';
-import birdIcon from '@/assets/images/icons/icon-bird.svg';
-import fishIcon from '@/assets/images/icons/icon-fish.svg';
-import mammalIcon from '@/assets/images/icons/icon-mammal.svg';
-import reptileIcon from '@/assets/images/icons/icon-reptile.svg';
+import allIcon from '@/assets/images/icons/icon-all.png';
+import amphibianIcon from '@/assets/images/icons/icon-amphibian.png';
+import birdIcon from '@/assets/images/icons/icon-bird.png';
+import fishIcon from '@/assets/images/icons/icon-fish.png';
+import mammalIcon from '@/assets/images/icons/icon-mammal.png';
+import reptileIcon from '@/assets/images/icons/icon-reptile.png';
 
 const ICON_MAP = {
   전체: allIcon,
