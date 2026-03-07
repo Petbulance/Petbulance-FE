78ab35d89e357472bb7b82e104149e5e14996227
yunseong0404 <333665@naver.com>
2026-03-07 16:47:49 +0900

    fix: 배포 테스트

--- src/pages/user/community/CommunityTabTemporary.jsx
+++ src/pages/user/community/CommunityTabTemporary.jsx
@@ -5,7 +5,7 @@ export function CommunityTabTemporary() {
     <div className="mt-10 flex flex-col px-6 text-center">
       <div className="mt-8">
         <h2 className="text-base font-semibold text-[#2DA969]">
-          커뮤니티 기능 준비중이에요!!!
+          커뮤니티 기능 준비중이에요!! 테스트중
         </h2>
 
         <p className="text-tertiary mt-3 text-sm">빠른 시일 내에 찾아뵐게요.</p>
